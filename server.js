c2965731ab2407a9a61075585b65d9b23b31d6e5
Imperium Prunum <adamson144144@gmail.com>
2018-08-21 17:17:28 +0200

    Compatibilization with heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000	;
+
 
 var app = express();
 
@@ -41,4 +43,6 @@ app.get('/about', (req,res) => {
 	});
 })
 */
-app.listen(3000);
+app.listen(port,()=>{
+	console.log(`Server working on port ${port}`);
+});
